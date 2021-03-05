7fa15344834e4d7dfe4f1b01c5a81df7023c72a1
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2021-03-04 23:36:15 -0500

    extract AWS setup to a function for easier testing

--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -1,4 +1,31 @@
-import { Storage } from "aws-amplify";
+import { Storage, Amplify } from "aws-amplify";
+import config from "../config";
+
+export function setupAws() {
+  Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+    },
+    Storage: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    },
+    API: {
+      endpoints: [
+        {
+          name: "notes",
+          endpoint: config.apiGateway.URL,
+          region: config.apiGateway.REGION,
+        },
+      ],
+    },
+  });
+}
 
 export async function s3Upload(file) {
   const filename = `${Date.now()}-${file.name}`;
