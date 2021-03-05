ba12aa27fb680d93b36506c53cd5f357457836f8
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2021-03-05 02:22:50 -0500

    write e2e tests for the spinner

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,41 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+import "@testing-library/cypress/add-commands";
+import "cypress-localstorage-commands";
+import { Auth } from "aws-amplify";
+import { cognito } from "../../src/config";
+
+/* Don't leak those secrets!
+ * Username and password stored in a .env
+ */
+const username = Cypress.env("username");
+const password = Cypress.env("password");
+
+const awsconfig = {
+  aws_user_pools_id: cognito.USER_POOL_ID,
+  aws_user_pools_web_client_id: cognito.APP_CLIENT_ID,
+};
+
+Auth.configure(awsconfig);
+
+/* Add a command to log in to Scratch */
+Cypress.Commands.add("login", () => {
+  cy.then(() => Auth.signIn(username, password)).then((cognitoUser) => {
+    const idToken = cognitoUser.signInUserSession.idToken.jwtToken;
+    const accessToken = cognitoUser.signInUserSession.accessToken.jwtToken;
+
+    const makeKey = (name) =>
+      `CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.${cognitoUser.username}.${name}`;
+
+    cy.setLocalStorage(makeKey("accessToken"), accessToken);
+    cy.setLocalStorage(makeKey("idToken"), idToken);
+    cy.setLocalStorage(
+      `CognitoIdentityServiceProvider.${cognitoUser.pool.clientId}.LastAuthUser`,
+      cognitoUser.username
+    );
+  });
+
+  cy.saveLocalStorage();
+});
